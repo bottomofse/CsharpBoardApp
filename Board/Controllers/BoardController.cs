7612c545e9cca9ba9929af91e45e84d0798ea458
developer <ddd98754@gmail.com>
2023-03-05 10:13:42 +0900

    掲示板アプリ

---
+++ Board/Controllers/BoardController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Board.Models;
+
+namespace Board.Controllers
+{
+    public class BoardController : Controller
+    {
+        private BoardDbContext db_;
+
+        public BoardController(): this(null)
+        {
+        }
+
+        public BoardController(BoardDbContext db)
+        {
+            db_ = db ?? new BoardDbContext();
+        }
+
+        // GET: Board
+        public ActionResult Index()
+        {
+            var model = new BoardListModel(db_);
+            return View(model);
+        }
+
+        // GET: Board/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Board/Create
+        [HttpPost]
+        public ActionResult Create(BoardCreateModel data)
+        {
+            db_.Boards.Add(new BoardEntity
+            {
+                Title = data.Title,
+                Text = data.Text
+            });
+            db_.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Board/Models/BoardDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Board.Models {
+
+    public class BoardEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public virtual ICollection<BoardPostEntity> Posts { get; set; }
+
+    }
+
+    public class BoardPostEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+    }
+
+    public class BoardListModel
+    {
+        public List<BoardEntity> Boards { get; private set; }
+
+        public BoardListModel(BoardDbContext db)
+        {
+            Boards = db.Boards.ToList();
+        }
+    }
+
+
+    public class BoardDbContext:DbContext
+    {
+        public BoardDbContext(): base("DefaultConnection")
+        {
+
+        }
+
+        public virtual DbSet<BoardEntity> Boards { get; set; }
+    }
+}
